104424cad84e5cec35eb4c4d03d7160877084774
Alex Zvoleff <azvoleff@gmail.com>
2014-09-29 17:02:43 -0400

    Update for latest init code.

--- 2_fit_model_jags.R
+++ 2_fit_model_jags.R
@@ -9,9 +9,14 @@ n_thin <- 10
 load("model_data.RData")
 load("init_data.RData")
 
-# Vector of zeros (see http://bit.ly/1uFTjlx)
+# Add a vector of zeros to support the "zeros trick" (see 
+# http://bit.ly/1uFTjlx)
 model_data$zeros <- matrix(0, nrow=model_data$n_tree, 
-                           ncol=model_data$n_periods)
+                           ncol=ncol(model_data$log_dbh))
+
+# Add transformations of vars:
+model_data$spi_sq <- model_data$spi^2
+model_data$WD_sq <- model_data$WD^2
 
 seed <- 1638
 
@@ -27,10 +32,13 @@ jags_params <- c("log_dbh_latent",
                  "b_jk",
                  "b_k")
 
+# Drop missing data indicators (not needed for JAGS)
+model_data <- model_data[!(names(model_data) %in% c("obs", "miss"))]
+
 jags_fit <- jags(data=model_data, inits=rep(init_data, 1),
                  parameters.to.save=jags_params, n.chains=1, 
                  n.burnin=n_burnin, n.iter=n_iter, n.thin=n_thin, 
-                 model.file=model_file)
+                 model.file=model_file, jags.seed=seed)
 
 print("finished running single JAGS chain")
 save(jags_fit_parallel, file="jags_fit_parallel.RData")
