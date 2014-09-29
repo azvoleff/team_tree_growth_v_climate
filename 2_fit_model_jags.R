1499432d059ae5b358e3652dc1968187cc4f33cf
Alex Zvoleff <azvoleff@gmail.com>
2014-09-29 17:20:28 -0400

    Update parallel code.

--- 2_fit_model_jags.R
+++ 2_fit_model_jags.R
@@ -1,9 +1,9 @@
 library(R2jags)
 
 model_file <- "full_model.bug" 
-n_chains <- 12
-n_burnin <- 12
-n_iter <- 1000
+n_chains <- 8
+n_burnin <- 10000
+n_iter <- 10000
 n_thin <- 10
 
 load("model_data.RData")
@@ -35,20 +35,20 @@ jags_params <- c("log_dbh_latent",
 # Drop missing data indicators (not needed for JAGS)
 model_data <- model_data[!(names(model_data) %in% c("obs", "miss"))]
 
+set.seed(seed)
 jags_fit <- jags(data=model_data, inits=rep(init_data, 1),
                  parameters.to.save=jags_params, n.chains=1, 
                  n.burnin=n_burnin, n.iter=n_iter, n.thin=n_thin, 
-                 model.file=model_file, jags.seed=seed)
+                 model.file=model_file)
 
 print("finished running single JAGS chain")
-save(jags_fit_parallel, file="jags_fit_parallel.RData")
+save(jags_fit, file="jags_fit.RData")
 
 
-jags_fit_parallel <- jags.parallel(data=model_data,
-                                   inits=rep(init_data, n_chains),
-                                   parameters.to.save=jags_params, 
-                                   model.file=model_file, n.chains=n_chains, 
-                                   n.burnin=n_burnin, n.iter=n_iter, 
-                                   n.thin=n_thin)
+jags_fit_p <- jags.parallel(data=model_data, inits=rep(init_data, n_chains), 
+                            parameters.to.save=jags_params, 
+                            model.file=model_file, n.chains=n_chains, 
+                            n.burnin=n_burnin, n.iter=n_iter, n.thin=n_thin, 
+                            jags.seed=seed)
 print("finished running JAGS chains in parallel")
-save(jags_fit_parallel, file="jags_fit_parallel.RData")
+save(jags_fit_p, file="jags_fit_parallel.RData")
