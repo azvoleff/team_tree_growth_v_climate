f883c907d3bd4009763c76ca227e13fa75e8ff82
Alex Zvoleff <azvoleff@gmail.com>
2014-10-01 17:58:44 -0400

    Finalize for new run on vertica1.

--- 2_fit_model_jags.R
+++ 2_fit_model_jags.R
@@ -1,8 +1,6 @@
 library(R2jags)
 
 model_file <- "full_model.bug" 
-n_chains <- 8
-n_iter <- 20000
 
 load("model_data.RData")
 load("init_data.RData")
@@ -29,16 +27,29 @@ model_data$WD_sq <- model_data$WD^2
 # Drop missing data indicators (not needed for JAGS)
 model_data <- model_data[!(names(model_data) %in% c("miss_indices", "obs_indices"))]
 
-set.seed(seed)
-jags_fit <- jags(data=model_data, inits=rep(init_data, 1),
-                 parameters.to.save=jags_params, n.chains=1, 
-                 n.iter=1000, model.file=model_file)
-print("finished running single JAGS chain")
-save(jags_fit, file="jags_fit_full.RData")
+# set.seed(seed)
+# seq_n_chains <- 10
+# jags_fit <- jags(data=model_data, inits=rep(init_data, seq_n_chains),
+#                  parameters.to.save=jags_params, n.chains=seq_n_chains, 
+#                  n.iter=10000, model.file=model_file)
+# print("finished running single JAGS chain")
+# save(jags_fit, file="jags_fit_full.RData")
 
-jags_fit_p <- jags.parallel(data=model_data, inits=rep(init_data, n_chains), 
+# jags.parallel only works when it is pulling the data from the R environment
+n_tree <- model_data$n_tree
+n_plot <- model_data$n_plot
+n_site <- model_data$n_site
+first_obs_period <- model_data$first_obs_period
+last_obs_period <- model_data$last_obs_period
+plot_ID <- model_data$plot_ID
+site_ID <- model_data$site_ID
+dbh <- model_data$dbh
+WD <- model_data$WD
+spi <- model_data$spi
+WD_sq <- model_data$WD_sq
+jags_fit_p <- jags.parallel(names(model_data), init_data,
                             parameters.to.save=jags_params, 
-                            model.file="full_model.bug", n.chains=n_chains, 
-                            n.iter=n_iter, jags.seed=seed)
+                            model.file="full_model.bug", n.chains=12, 
+                            n.iter=100000, jags.seed=70276)
 print("finished running JAGS chains in parallel")
 save(jags_fit_p, file="jags_fit_full_parallel.RData")
