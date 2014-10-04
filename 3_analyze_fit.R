46b7449a8c5d0837c69c05e8d01256593c9d341a
Alex Zvoleff <azvoleff@gmail.com>
2014-10-04 12:00:33 -0400

    Add variable list.

--- 3_analyze_fit.R
+++ 3_analyze_fit.R
@@ -3,7 +3,25 @@ library(coda)
 
 load("jags_fit_full.RData")
 
-traceplot(jags_fit)
+trace_vars <- c("intercept",
+                "slp_dbh",
+                "slp_dbh_sq",
+                "slp_WD",
+                "slp_WD_sq",
+                "slp_spi",
+                "inter_spi_dbh",
+                "inter_spi_WD",
+                "obs_sigma",
+                "proc_sigma",
+                "sigma_ijk",
+                "sigma_jk",
+                "sigma_k",
+                "sigma_t",
+                "sigma_g",
+                "sigma_slp_spi_g",
+                "rho")
+
+traceplot(jags_fit, trace_vars)
 
 jags_fit_mcmc <- as.mcmc(jags_fit)
 
