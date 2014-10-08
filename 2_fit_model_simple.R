0677f8ce9b66fb18a04451695f609271594de5d5
Alex Zvoleff <azvoleff@gmail.com>
2014-10-08 14:52:07 -0400

    Remove unneeded var in simple fit code.

--- 2_fit_model_simple.R
+++ 2_fit_model_simple.R
@@ -21,19 +21,18 @@ monitored <- c("int",
 
 init_data <- init_data[names(init_data) %in% c("dbh_latent")]
 
-model_data$W <- diag(4)
 model_data$WD_sq <- model_data$WD^2
 # Drop missing data indicators (not needed for JAGS)
 model_data <- model_data[!(names(model_data) %in% c("miss_indices", "obs_indices"))]
 
-seq_n_chains <- 1
-jags_fit <- run.jags(model=model_file, monitor=monitored, data=model_data, 
-                     inits=rep(list(init_data), seq_n_chains), 
-                     n.chains=seq_n_chains, adapt=100, burnin=100, 
-                     sample=100, thin=2)
-print("finished running single JAGS chain")
-run_id <- paste0(Sys.info()[4], format(Sys.time(), "_%Y%m%d-%H%M%S"))
-save(jags_fit, file=paste0("simple_model_fit_", run_id, ".RData"))
+# seq_n_chains <- 1
+# jags_fit <- run.jags(model=model_file, monitor=monitored, data=model_data, 
+#                      inits=rep(list(init_data), seq_n_chains), 
+#                      n.chains=seq_n_chains, adapt=100, burnin=100, 
+#                      sample=100, thin=2)
+# print("finished running single JAGS chain")
+# run_id <- paste0(Sys.info()[4], format(Sys.time(), "_%Y%m%d-%H%M%S"))
+# save(jags_fit, file=paste0("simple_model_fit_", run_id, ".RData"))
 
 jags_fit <- run.jags(model=model_file, monitor=monitored, data=model_data, 
                      inits=rep(list(init_data), 4), n.chains=4, 
@@ -42,4 +41,3 @@ jags_fit <- run.jags(model=model_file, monitor=monitored, data=model_data,
 print("finished running JAGS chains in parallel")
 run_id <- paste0(Sys.info()[4], format(Sys.time(), "_%Y%m%d-%H%M%S"))
 save(jags_fit, file=paste0("simple_model_fit_parallel_", run_id, ".RData"))
-
