86f0154f5a60a7325f20508dfa1c7394beb9f190
Alex Zvoleff <azvoleff@gmail.com>
2014-10-17 16:48:28 -0400

    Fix error in loading init_data.

--- 3_fit_model_full_jags.R
+++ 3_fit_model_full_jags.R
@@ -7,7 +7,7 @@ load.module("glm")
 model_file <- "full_model.bug" 
 
 load("model_data_wide.RData")
-load("init_data.RData")
+load("init_data_with_ranefs.RData")
 
 monitored <- c("int",
                "slp_dbh",
@@ -26,8 +26,6 @@ monitored <- c("int",
                "sigma_B",
                "rho_B")
 
-init_data <- init_data[names(init_data) %in% c("dbh_latent")]
-
 model_data$K <- 5
 model_data$W <- diag(model_data$K)
 model_data$WD_sq <- model_data$WD^2
@@ -38,8 +36,8 @@ model_data <- model_data[!(names(model_data) %in% c("miss_indices", "obs_indices
 # seq_n_chains <- 1
 # jags_fit <- run.jags(model=model_file, monitor=monitored, data=model_data, 
 #                      inits=rep(list(init_data), seq_n_chains), 
-#                      n.chains=seq_n_chains, adapt=100, burnin=100, 
-#                      sample=100, thin=2)
+#                      n.chains=seq_n_chains, adapt=50, burnin=50, 
+#                      sample=50)
 # print("finished running single JAGS chain")
 # run_id <- paste0(Sys.info()[4], format(Sys.time(), "_%Y%m%d-%H%M%S"))
 # save(jags_fit, file=paste0("full_model_fit_", run_id, ".RData"))
@@ -50,6 +48,7 @@ init_data$slp_dbh_sq <- 0
 init_data$slp_WD <- 0
 init_data$slp_WD_sq <- 0
 init_data$slp_mcwd <- 0
+init_data$slp_mcwd_sq <- 0
 
 jags_fit <- run.jags(model=model_file, monitor=monitored,
                      data=model_data, inits=rep(list(init_data), 4),
