a97db151ea2bd8952e321864f9e24e5cb4635275
Alex Zvoleff <azvoleff@gmail.com>
2014-10-20 17:15:50 -0400

    Add corrplot code.

--- 5_1_full_model_analyze_fit.R
+++ 5_1_full_model_analyze_fit.R
@@ -3,7 +3,9 @@ library(coda)
 library(dplyr)
 library(reshape2)
 library(ggplot2)
+#library(GGally)
 library(corrplot)
+library(grid) # for unit
 
 img_dpi <- 300
 
@@ -27,9 +29,9 @@ plot_estimates <- function(mcmc_ests, pars, pars_pretty=NULL, xmin=NULL, xmax=NU
         }
     }
     mcmc_ests <- group_by(mcmc_ests, variable) %>%
-        summarize(median=median(value),
-                  q2pt5=quantile(value, .025),
-                  q97pt5=quantile(value, .975))
+    summarize(median=median(value),
+              q2pt5=quantile(value, .025),
+              q97pt5=quantile(value, .975))
 
     if (is.null(pars_pretty)) {
         mcmc_ests$variable <- ordered(mcmc_ests$variable, 
@@ -60,19 +62,19 @@ plot_estimates <- function(mcmc_ests, pars, pars_pretty=NULL, xmin=NULL, xmax=NU
 load("jags_fit_full_model_fixefs.RData")
 
 fixefs_names <- c("int",
-                 "slp_mcwd",
-                 "slp_mcwd_sq",
-                 "slp_dbh",
-                 "slp_dbh_sq",
-                 "slp_WD",
-                 "slp_WD_sq")
+                  "slp_mcwd",
+                  "slp_mcwd_sq",
+                  "slp_dbh",
+                  "slp_dbh_sq",
+                  "slp_WD",
+                  "slp_WD_sq")
 fixefs_names_pretty <- c("Intercept",
-                        "MCWD",
-                        expression(MCWD^2),
-                        "Diameter",
-                        expression(Diameter^2),
-                        "Density",
-                        expression(Density^2))
+                         "MCWD",
+                         expression(MCWD^2),
+                         "Diameter",
+                         expression(Diameter^2),
+                         "Density",
+                         expression(Density^2))
 fixefs_scaling <- c(dbh_sd,
                     (dbh_sd/mcwd_sd) * 100, # Convert from mm to 10s of cm
                     (dbh_sd/mcwd_sd) * 100, # Convert from mm to 10s of cm
@@ -163,15 +165,15 @@ ranefs_g_rho_names <- c("rho_B_g[1,2]",
                         "rho_B_g[3,5]",
                         "rho_B_g[4,5]")
 ranefs_g_rho_names_pretty <- c(expression(rho[list(int,MCWD)]),
-                        expression(rho[list(int,MCWD^2)]),
-                        expression(rho[list(int,DBH[t-1])]),
-                        expression(rho[list(int,DBH[t-1]^2)]),
-                        expression(rho[list(MCWD,MCWD^2)]),
-                        expression(rho[list(MCWD,DBH[t-1])]),
-                        expression(rho[list(MCWD,DBH[t-1]^2)]),
-                        expression(rho[list(MCWD^2,DBH[t-1])]),
-                        expression(rho[list(MCWD^2,DBH[t-1]^2)]),
-                        expression(rho[list(DBH[t-1],DBH[t-1]^2)]))
+                               expression(rho[list(int,MCWD^2)]),
+                               expression(rho[list(int,DBH[t-1])]),
+                               expression(rho[list(int,DBH[t-1]^2)]),
+                               expression(rho[list(MCWD,MCWD^2)]),
+                               expression(rho[list(MCWD,DBH[t-1])]),
+                               expression(rho[list(MCWD,DBH[t-1]^2)]),
+                               expression(rho[list(MCWD^2,DBH[t-1])]),
+                               expression(rho[list(MCWD^2,DBH[t-1]^2)]),
+                               expression(rho[list(DBH[t-1],DBH[t-1]^2)]))
 
 # gelman.diag(ranefs_g_rho)
 # ranefs_g_rho <- window(ranefs_g_rho, start=9001, end=15000, thin=50)
@@ -183,4 +185,83 @@ plot_estimates(ranefs_g_rho_comb, ranefs_g_rho_names,
                ranefs_g_rho_names_pretty, xmin=-1, xmax=1)
 ggsave("growth_model_ranefs_g_rho.png", width=3, height=6, dpi=img_dpi)
 
-corrplot
+ranef_names <- c("sigma[int,g]", "sigma[MCWD,g]", 
+                 "sigma[MCWD^2,g]", "sigma[DBH,g]", 
+                 "sigma[DBH^2,g]")
+corr_matrix <- matrix(apply(ranefs_g_rho_comb, 2, mean), nrow=5)
+colnames(corr_matrix) <- ranef_names
+rownames(corr_matrix) <- ranef_names
+corr_matrix <- t(corr_matrix*lower.tri(corr_matrix))
+corr_matrix_long <- melt(corr_matrix)
+corr_matrix_long$Var1 <- ordered(corr_matrix_long$Var1, 
+                                 levels=(unique(corr_matrix_long$Var1)))
+corr_matrix_long$Var2 <- ordered(corr_matrix_long$Var2, 
+                                 levels=rev(unique(corr_matrix_long$Var2)))
+corr_matrix_long$value[is.na(corr_matrix_long$value)] <- 0
+corr_labels <- data.frame(label=c("sigma[int,g]", "sigma[MCWD,g]", 
+                                  "sigma[MCWD^2,g]", "sigma[DBH,g]", 
+                                  "sigma[DBH^2,g]"))
+corr_labels$xpos=seq(1, length.out=nrow(corr_labels))
+corr_labels$ypos=seq(nrow(corr_labels), by=-1, length.out=nrow(corr_labels))
+ggplot(corr_matrix_long, aes(Var1, Var2)) +
+    geom_tile(fill="white") +
+    geom_point(aes(x=Var1, y=Var2, colour=value, size=abs(value)), data=corr_matrix_long[corr_matrix_long$value != 0, ]) +
+    scale_size_area(max_size=20, guide=FALSE) +
+    theme_bw(base_size=10) +
+    scale_colour_gradientn(expression(rho),
+                         colours=c('firebrick4', 'darkorange3', 'white', 'steelblue2', 'dodgerblue4'),
+                         limits=c(-1, 1)) +
+    scale_fill_gradientn(expression(rho),
+                         colours=c('firebrick4', 'darkorange3', 'white', 'steelblue2', 'dodgerblue4'),
+                         limits=c(-1, 1)) +
+    theme(axis.text=element_blank(), axis.ticks=element_blank(),
+          axis.title=element_blank(), 
+          panel.background=element_blank(), panel.border=element_blank(),
+          panel.grid=element_blank(),
+          plot.background=element_blank(), 
+          legend.title.align=.2, legend.position=c(.75, .75),
+          plot.margin=unit(c(.1, .1, .1, .1), 'cm'),
+          legend.direction="horizontal") +
+    geom_text(aes(label=label, x=xpos, y=ypos, hjust=.5, vjust=.5), parse=TRUE, 
+              data=corr_labels) +
+    geom_text(aes(label=round(value, 2), x=Var1, y=Var2, hjust=.5, vjust=.5), 
+              parse=TRUE, data=corr_matrix_long[corr_matrix_long$value != 0, ])
+ggsave("growth_model_ranefs_g_rho_corrplot.png", width=4, height=4, dpi=img_dpi)
+
+corrplot.mixed(corr_matrix)
+
+# gelman.diag(ranefs_g_sigma)
+# ranefs_g_sigma <- window(ranefs_g_sigma, start=9001, end=15000, thin=50)
+#plot(ranefs_g_sigma, ask=TRUE)
+#autocorr.plot(ranefs_g_sigma)
+
+# D <- round(corr_matrix, 1)
+# D <- D * lower.tri(D)
+# D <- as.data.frame(D)
+# D <- data.frame(row = colnames(corr_matrix), D)
+# D <- melt(D, id.vars = "row")
+# M <- corr_matrix * lower.tri(corr_matrix)
+# M <- as.data.frame(M)
+# M <- data.frame(row = colnames(corr_matrix), M)
+# names(M) <- c("row", colnames(corr_matrix))
+# M <- melt(M, id.vars = "row")
+# M$value[M$value == 0] <- NA
+# s <- seq(-1, 1, by = 0.25)
+# M$value <- cut(M$value, breaks = s, include.lowest = TRUE, 
+#                label = cut(s, breaks = s)[-1])
+# M$row <- factor(M$row, levels=corr_mat_names)
+# M$num <- as.numeric(M$value)
+# diag <- subset(M, row == variable)
+# po.nopanel <- list(theme(panel.background = element_blank(), 
+#                          panel.grid.minor = element_blank(), panel.grid.major = element_blank(), 
+#                          axis.text.x = element_text(angle = -90)))
+# p = ggplot(M, aes(row, variable)) 
+# p = p + scale_colour_brewer(expression(rho), palette = "RdYlGn") + 
+#     scale_size_area(expression(rho), max_size = 6,
+#                     labels = 
+#                     levels(M$value)[table(M$value) > 0]) +
+#     geom_point(aes(size = num, colour = value))
+# p = p + geom_text(data = diag, aes(label = variable)) +
+#     scale_x_discrete(breaks = NULL) +
+#     scale_y_discrete(breaks = NULL) + 
+#     labs(x = NULL, y = NULL) + coord_equal() + po.nopanel
