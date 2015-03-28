bba7ed4370bc4eb7eb03934063339cbd7136a9c0
Alex Zvoleff <azvoleff@conservation.org>
2015-03-28 17:21:17 -0400

    Reorganize vars.

--- 0_settings.R
+++ 0_settings.R
@@ -1,14 +1,12 @@
+# temp_var <- 'tmn_meanannual'
+# precip_var <- 'mcwd_run12'
+note <- ""
+#note <- "highelev"
+
 temp_vars <- c("tmn_meanannual", "tmp_meanannual", "tmx_meanannual")
 #precip_vars <- c("mcwd_run12", "spi_24")
 precip_vars <- c("mcwd_run12")
 model_types <- c("full", "testing")
-
-# temp_var <- 'tmn_meanannual'
-# precip_var <- 'mcwd_run12'
-# model_type <- 'full'
-
-note <- ""
-#note <- "highelev"
 
 prefixes <- c('D:/azvoleff/Data', # CI-TEAM
               'H:/Data', # Buffalo drive
