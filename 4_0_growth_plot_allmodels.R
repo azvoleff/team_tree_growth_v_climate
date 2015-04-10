e4b1b13d9b1e8372a55b6724b5be9a49adf5d954
Alex Zvoleff <azvoleff@gmail.com>
2015-04-10 17:46:46 -0400

    Fix for latest models.

--- 4_0_growth_plot_allmodels.R
+++ 4_0_growth_plot_allmodels.R
@@ -8,10 +8,12 @@ prefix <- prefixes[match(TRUE, unlist(lapply(prefixes, function(x) file_test('-d
 
 library(mcmcplots)
 library(runjags)
+library(ggmcmc)
+library(ggthemes)
 library(coda)
 
-start_val <- 10000
-thin_val <- 8
+start_val <- 30000
+thin_val <- 10
 
 plot_mcmc <- function(fit, params, title) {
     mcmc_results <- as.mcmc.list(fit, c(params))
@@ -24,34 +26,27 @@ plot_mcmc <- function(fit, params, title) {
 ## Simple model (no random effects)
 ##
 
-# mcmc_results <- as.mcmc.list(jags_fit)
-# mcmc_results <- window(mcmc_results, start=start_val, thin=thin_val)
-# mcmcplot(mcmc_results, regex=c("B\\[", "mu_B_g", "sigma_"))
+plot_simple_model <- function(title) {
+    plot_mcmc(jags_fit, c("B", "mu_B_g", "sigma_", "int_jk", "ink_k"), title)
+}
 
+### Minimum temperature
+load(file.path(prefix, "TEAM", "Tree_Growth", "MCMC_Chains", "simple", 
+               "simple_tmn.RData"))
+plot_simple_model('simple min temp')
 
-# plot_simple_model <- function(title) {
-#     plot_mcmc(jags_fit, "B[", title)
-#     plot_mcmc(jags_fit, "sigma_", title)
-#     #plot_mcmc(jags_fit, "^int_k", title)
-# }
-#
-# ### Mean temperature
-# load(file.path(prefix, "TEAM", "Tree_Growth", "MCMC_Chains", "simple", 
-#                "simple_tmp.RData"))
-# plot_simple_model('simple mean temp')
-#
-# ### Minimum temperature
-# load(file.path(prefix, "TEAM", "Tree_Growth", "MCMC_Chains", "simple", 
-#                "simple_tmn.RData"))
-# plot_simple_model('simple min temp')
-#
-# ### Maximum temperature
-# load(file.path(prefix, "TEAM", "Tree_Growth", "MCMC_Chains", "simple", 
-#                "simple_tmx.RData"))
-# plot_simple_model('simple max temp')
+### Mean temperature
+load(file.path(prefix, "TEAM", "Tree_Growth", "MCMC_Chains", "simple", 
+               "simple_tmp.RData"))
+plot_simple_model('simple mean temp')
+
+### Maximum temperature
+load(file.path(prefix, "TEAM", "Tree_Growth", "MCMC_Chains", "simple", 
+               "simple_tmx.RData"))
+plot_simple_model('simple max temp')
 
 ###############################################################################
-## Smaller model (UDZ, BIF, VB only)
+## Model with only sites with large elevation gradients (UDZ, BIF, VB only)
 
 # plot_small_model <- function(title) {
 #     plot_mcmc(jags_fit, c("B[1]", "B[2]"), title)
@@ -96,18 +91,42 @@ load(file.path(prefix, "TEAM", "Tree_Growth", "MCMC_Chains", "interact",
                "interact_tmx.RData"))
 plot_full_interact_model("interact max temp")
 
+# interact_labels_mu_B_g <- data.frame(Parameter=paste0('mu_B_g[', 1:12, ']'),
+#     Label=c("int",
+#             "P",
+#             "P^2",
+#             "T",
+#             "T^2",
+#             "D",
+#             "D^2",
+#             "W*P",
+#             "D*P",
+#             "W*T",
+#             "D*T",
+#             "E"))
+#
+# mu_B_g_vals <- ggs(as.mcmc.list(jags_fit, "mu_B_g"), par_labels=interact_labels_mu_B_g)
+# ggs_caterpillar(mu_B_g_vals)
+# ggs_caterpillar(mu_B_g_vals) + theme_tufte()
+# ggs_caterpillar(mu_B_g_vals) + theme_solarized_2()
+
 ###############################################################################
 ## Full model (correlated random effects)
 plot_full_correlated_model <- function(title) {
     plot_mcmc(jags_fit, c("B[1]", "B[2]", "mu_B_g", "sigma_", "B_T_"), title)
 }
 
-### Minimum temperature
+### Min temperature
 load(file.path(prefix, "TEAM", "Tree_Growth", "MCMC_Chains", "correlated", 
                "correlated_tmn.RData"))
 plot_full_correlated_model("correlated min temp")
 
 ### Mean temperature
+load(file.path(prefix, "TEAM", "Tree_Growth", "MCMC_Chains", "correlated", 
+               "correlated_tmp.RData"))
+plot_full_correlated_model("correlated mean temp")
 
 ### Maximum temperature
-
+load(file.path(prefix, "TEAM", "Tree_Growth", "MCMC_Chains", "correlated", 
+               "correlated_tmx.RData"))
+plot_full_correlated_model("correlated max temp")
