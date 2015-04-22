ef51026fcac9d2ae788a88ce5c349d75ec511e65
Alex Zvoleff <azvoleff@conservation.org>
2015-04-22 13:50:48 -0400

    Finalize extraction code.

--- 4_2_extract_coefs.R
+++ 4_2_extract_coefs.R
@@ -34,6 +34,7 @@ plot_dpi <- 300
 # Functions to "destandardize" standardized coefficients in an array of MCMC 
 # results
 destd <- function(d, rows, multiplier) {
+    stopifnot(sum(rows) > 0)
     d[rows, ]$value <- d[rows, ]$value * multiplier
     return(d)
 }
@@ -43,13 +44,13 @@ destandardize <- function(d, model_type, temp_var) {
                                        temp_var, 
                                        "_meanannual-mcwd_run12.RData")))
     if (model_type == "simple") {
-        d <- destd(d, d$Parameter_Base == "B_g" & d$col_ID == 1, dbh_sd)
-        d <- destd(d, d$Parameter_Base == "B_g" & d$col_ID == 2, (dbh_sd/precip_sd) * 100) # Convert from mm to 10s of cm
-        d <- destd(d, d$Parameter_Base == "B_g" & d$col_ID == 3, (dbh_sd/precip_sd) * 100) # Convert from mm to 10s of cm
-        d <- destd(d, d$Parameter_Base == "B_g" & d$col_ID == 4, dbh_sd/temp_sd)
-        d <- destd(d, d$Parameter_Base == "B_g" & d$col_ID == 5, dbh_sd/temp_sd)
-        d <- destd(d, d$Parameter_Base == "B_g" & d$col_ID == 6, dbh_sd/dbh_sd)
-        d <- destd(d, d$Parameter_Base == "B_g" & d$col_ID == 7, dbh_sd/dbh_sd)
+        d <- destd(d, d$Parameter_Base == "B" & d$row_ID == 1, dbh_sd)
+        d <- destd(d, d$Parameter_Base == "B" & d$row_ID == 2, (dbh_sd/precip_sd) * 100) # Convert from mm to 10s of cm
+        d <- destd(d, d$Parameter_Base == "B" & d$row_ID == 3, (dbh_sd/precip_sd) * 100) # Convert from mm to 10s of cm
+        d <- destd(d, d$Parameter_Base == "B" & d$row_ID == 4, dbh_sd/temp_sd)
+        d <- destd(d, d$Parameter_Base == "B" & d$row_ID == 5, dbh_sd/temp_sd)
+        d <- destd(d, d$Parameter_Base == "B" & d$row_ID == 6, 1)
+        d <- destd(d, d$Parameter_Base == "B" & d$row_ID == 7, 1)
         d <- destd(d, d$Parameter_Base == "int_jk", dbh_sd)
         d <- destd(d, d$Parameter_Base == "int_k", dbh_sd)
         d <- destd(d, d$Parameter_Base == "int_t", dbh_sd)
@@ -59,18 +60,20 @@ destandardize <- function(d, model_type, temp_var) {
         d <- destd(d, d$Parameter_Base == "sigma_int_jk", dbh_sd)
         d <- destd(d, d$Parameter_Base == "sigma_int_k", dbh_sd)
         d <- destd(d, d$Parameter_Base == "sigma_int_t", dbh_sd)
+        d <- destd(d, d$Parameter_Base == "B_k", dbh_sd)
     } else if (model_type == "interact") {
         B_g_rows <- d$Parameter_Base == "B_g"
-        mu_B_g_rows <- d$Parameter_Base == "mu_B_g"
-        d <- destd(d, (mu_B_g_rows & d$row_ID == 1) | (B_g_rows & d$col_ID == 1), dbh_sd)
-        d <- destd(d, (mu_B_g_rows & d$row_ID == 2) | (B_g_rows & d$col_ID == 2), (dbh_sd/precip_sd) * 100) # Convert from mm to 10s of cm
-        d <- destd(d, (mu_B_g_rows & d$row_ID == 3) | (B_g_rows & d$col_ID == 3), (dbh_sd/precip_sd) * 100) # Convert from mm to 10s of cm
-        d <- destd(d, (mu_B_g_rows & d$row_ID == 4) | (B_g_rows & d$col_ID == 4), dbh_sd/temp_sd)
-        d <- destd(d, (mu_B_g_rows & d$row_ID == 5) | (B_g_rows & d$col_ID == 5), dbh_sd/temp_sd)
-        d <- destd(d, (mu_B_g_rows & d$row_ID == 6) | (B_g_rows & d$col_ID == 6), 1)
-        d <- destd(d, (mu_B_g_rows & d$row_ID == 7) | (B_g_rows & d$col_ID == 7), 1)
-        d <- destd(d, (mu_B_g_rows & d$row_ID == 8) | (B_g_rows & d$col_ID == 8), 1/precip_sd)
-        d <- destd(d, (mu_B_g_rows & d$row_ID == 9) | (B_g_rows & d$col_ID == 9), (1/temp_sd) * 100)
+        B_g_sigma_mu_rows <- d$Parameter_Base %in% c("mu_B_g", "sigma_B_g")
+        d$value_destd <- NA
+        d <- destd(d, (B_g_sigma_mu_rows & d$row_ID == 1) | (B_g_rows & d$col_ID == 1), dbh_sd)
+        d <- destd(d, (B_g_sigma_mu_rows & d$row_ID == 2) | (B_g_rows & d$col_ID == 2), (dbh_sd/precip_sd) * 100) # Convert from mm to 10s of cm
+        d <- destd(d, (B_g_sigma_mu_rows & d$row_ID == 3) | (B_g_rows & d$col_ID == 3), (dbh_sd/precip_sd) * 100) # Convert from mm to 10s of cm
+        d <- destd(d, (B_g_sigma_mu_rows & d$row_ID == 4) | (B_g_rows & d$col_ID == 4), dbh_sd/temp_sd)
+        d <- destd(d, (B_g_sigma_mu_rows & d$row_ID == 5) | (B_g_rows & d$col_ID == 5), dbh_sd/temp_sd)
+        d <- destd(d, (B_g_sigma_mu_rows & d$row_ID == 6) | (B_g_rows & d$col_ID == 6), 1)
+        d <- destd(d, (B_g_sigma_mu_rows & d$row_ID == 7) | (B_g_rows & d$col_ID == 7), 1)
+        d <- destd(d, (B_g_sigma_mu_rows & d$row_ID == 8) | (B_g_rows & d$col_ID == 8), 1/precip_sd)
+        d <- destd(d, (B_g_sigma_mu_rows & d$row_ID == 9) | (B_g_rows & d$col_ID == 9), (1/temp_sd) * 100)
         d <- destd(d, d$Parameter_Base == "int_jk", dbh_sd)
         d <- destd(d, d$Parameter_Base == "int_k", dbh_sd)
         d <- destd(d, d$Parameter_Base == "int_t", dbh_sd)
@@ -80,16 +83,17 @@ destandardize <- function(d, model_type, temp_var) {
         d <- destd(d, d$Parameter_Base == "sigma_int_jk", dbh_sd)
         d <- destd(d, d$Parameter_Base == "sigma_int_k", dbh_sd)
         d <- destd(d, d$Parameter_Base == "sigma_int_t", dbh_sd)
+        d <- destd(d, d$Parameter_Base == "B_k", dbh_sd)
     } else if (model_type == "correlated") {
         B_g_rows <- d$Parameter_Base == "B_g"
-        mu_B_g_rows <- d$Parameter_Base == "mu_B_g"
-        d <- destd(d, (mu_B_g_rows & d$row_ID == 1) | (B_g_rows & d$col_ID == 1), dbh_sd)
-        d <- destd(d, (mu_B_g_rows & d$row_ID == 2) | (B_g_rows & d$col_ID == 2), (dbh_sd/precip_sd) * 100) # Convert from mm to 10s of cm
-        d <- destd(d, (mu_B_g_rows & d$row_ID == 3) | (B_g_rows & d$col_ID == 3), (dbh_sd/precip_sd) * 100) # Convert from mm to 10s of cm
-        d <- destd(d, (mu_B_g_rows & d$row_ID == 4) | (B_g_rows & d$col_ID == 4), dbh_sd/temp_sd)
-        d <- destd(d, (mu_B_g_rows & d$row_ID == 5) | (B_g_rows & d$col_ID == 5), dbh_sd/temp_sd)
-        d <- destd(d, (mu_B_g_rows & d$row_ID == 6) | (B_g_rows & d$col_ID == 6), dbh_sd/dbh_sd)
-        d <- destd(d, (mu_B_g_rows & d$row_ID == 7) | (B_g_rows & d$col_ID == 7), dbh_sd/dbh_sd)
+        B_g_sigma_mu_rows <- d$Parameter_Base %in% c("mu_B_g", "sigma_B_g")
+        d <- destd(d, (B_g_sigma_mu_rows & d$row_ID == 1) | (B_g_rows & d$col_ID == 1), dbh_sd)
+        d <- destd(d, (B_g_sigma_mu_rows & d$row_ID == 2) | (B_g_rows & d$col_ID == 2), (dbh_sd/precip_sd) * 100) # Convert from mm to 10s of cm
+        d <- destd(d, (B_g_sigma_mu_rows & d$row_ID == 3) | (B_g_rows & d$col_ID == 3), (dbh_sd/precip_sd) * 100) # Convert from mm to 10s of cm
+        d <- destd(d, (B_g_sigma_mu_rows & d$row_ID == 4) | (B_g_rows & d$col_ID == 4), dbh_sd/temp_sd)
+        d <- destd(d, (B_g_sigma_mu_rows & d$row_ID == 5) | (B_g_rows & d$col_ID == 5), dbh_sd/temp_sd)
+        d <- destd(d, (B_g_sigma_mu_rows & d$row_ID == 6) | (B_g_rows & d$col_ID == 6), 1)
+        d <- destd(d, (B_g_sigma_mu_rows & d$row_ID == 7) | (B_g_rows & d$col_ID == 7), 1)
         d <- destd(d, d$Parameter_Base == "int_jk", dbh_sd)
         d <- destd(d, d$Parameter_Base == "int_k", dbh_sd)
         d <- destd(d, d$Parameter_Base == "int_t", dbh_sd)
@@ -99,28 +103,32 @@ destandardize <- function(d, model_type, temp_var) {
         d <- destd(d, d$Parameter_Base == "sigma_int_jk", dbh_sd)
         d <- destd(d, d$Parameter_Base == "sigma_int_k", dbh_sd)
         d <- destd(d, d$Parameter_Base == "sigma_int_t", dbh_sd)
+        d <- destd(d, d$Parameter_Base == "B_k", dbh_sd)
     } else {
         stop('unrecognized model type')
     }
     return(d)
 }
 
+# model_type <- "interact"
+# temp_var <- "tmn"
+
 start_val <- 40000
 thin_val <- 100
-foreach(model_type=c('interact', 'correlated', 'simple')) %do% {
+foreach(model_type=c('simple')) %do% {
     # Note the below is not parallel since running it in parallel breaks the Rcpp 
     # function.
-    params <- foreach(model_type==c('interact', 'correlated', 'simple'),
-                    temp_var=c('tmn', 'tmp', 'tmx'), .combine=rbind, .inorder=FALSE,
-                        .packages=c('runjags', 'ggmcmc', 'mcmcplots', 'Rcpp',
-                                    'dplyr')) %do% {
+    params <- foreach(temp_var=c('tmn', 'tmp', 'tmx'), .combine=rbind, 
+                      .inorder=FALSE,
+                      .packages=c('runjags', 'ggmcmc', 'mcmcplots', 'Rcpp',
+                                  'dplyr')) %do% {
+        message(paste0("Processing ", model_type, "/", temp_var))
         load(file.path(prefix, "TEAM", "Tree_Growth", "MCMC_Chains", model_type,
                        paste0(model_type, '_', temp_var, '.RData')))
         jags_fit <- window(as.mcmc.list(jags_fit), start=start_val, thin=thin_val)
         these_params <- ggs(jags_fit)
 
         ids <- jags_param_ids(these_params$Parameter)
-
         these_params <- cbind(these_params, ids)
 
         these_params <- destandardize(these_params, model_type, temp_var)
@@ -128,5 +136,5 @@ foreach(model_type=c('interact', 'correlated', 'simple')) %do% {
         these_params$Model <- temp_var 
         return(tbl_df(these_params))
     }
-    save(params, file=paste0(model_type, "parameter_estimates.RData"))
+    save(params, file=paste0("parameter_estimates_", model_type, ".RData"))
 }
