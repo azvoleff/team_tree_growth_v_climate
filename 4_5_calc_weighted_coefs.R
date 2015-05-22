6c08f35fa953f2a8f872e7a69485a8c1176cae7c
Alex Zvoleff <azvoleff@conservation.org>
2015-05-22 16:02:41 -0400

    Fix calculation of weighted coefs from postgres.

--- 4_5_calc_weighted_coefs.R
+++ 4_5_calc_weighted_coefs.R
@@ -6,6 +6,10 @@ library(foreach)
 library(dplyr)
 library(matrixStats)
 library(RPostgreSQL)
+library(doParallel)
+
+cl <- makeCluster(3)
+registerDoParallel(cl)
 
 pgsqlpwd <- as.character(read.table('~/pgsqlpwd')[[1]])
 pgsqluser <- as.character(read.table('~/pgsqluser')[[1]])
@@ -25,40 +29,6 @@ nThin <- 100
 # What burn-in was used? This does not change the thinning - it only is used to 
 # properly align the plots.
 nBurnin  <- 100000
-
-# genus_weights <- group_by(merged, genus_ID) %>%
-#     summarize(n=n()) %>%
-#     ungroup() %>%
-#     mutate(weight=n/sum(n)) %>%
-#     select(-n) %>%
-#     arrange(desc(weight))
-
-pg_src <- src_postgres('tree_growth', user=pgsqluser, password=pgsqlpwd)
-
-stems <- tbl(pg_src, paste0('stems'))
-params <- tbl(pg_src, 'interact')
-
-stems %>%
-    group_by(model, site_id, plot_id, genus_id) %>%
-    # Count num indiv of each genus within plot
-    summarize(n=n()) %>%
-    ungroup() %>%
-    group_by(model, site_id, plot_id) %>%
-    # Convert counts to plot-level weights
-    mutate(weight=n/sum(n)) %>%
-    collect() %>%
-    group_by(model, site_id, genus_id) %>%
-    # Convert plot-level weights to site-level weights for each genus
-    summarise(weight=sum(weight)) %>%
-    group_by(model, site_id) %>%
-    # Normalize weights
-    mutate(weight=weight/sum(weight)) -> genus_weights
-
-genus_weights <- copy_to(pg_src, genus_weights)
-
-# Check that weighting worked correctly (totals should be three as there are 
-# three models:
-#group_by(genus_weights, site_id) %>% summarise(sum(weight))
 
 caterpillar <- function(mods, labels=NULL) {
     cis <- group_by(mods, model) %>%
@@ -106,25 +76,65 @@ caterpillar <- function(mods, labels=NULL) {
 ###############################################################################
 ## Full model (interactions, uncorrelated random effects)
 
-params <- tbl(pg_src, 'interact')
+pg_src <- src_postgres('tree_growth', user=pgsqluser, password=pgsqlpwd)
 
-B_g_params <- filter(params, parameter_base == 'B_g') %>% 
-    rename(genus_id=row_id, param_id=col_id)
-    left_join(genus_weights) %>%
-    mutate(param=sql("parameter_base || '_' || param_id"),
-           estimate_id=sql("chain || '_' || iteration")) %>%
-    arrange(model, param, estimate_id, genus_id)
+stems <- tbl(pg_src, paste0('stems'))
 
-B_g_betas <- foreach(this_model=c('tmn', 'tmp', 'tmx'), 
-                              .combine=rbind) %do% {
-    these_B_g_betas <- filter(B_g_params, model == this_model) %>%
+# Calculate site-level genus weights, weighting all plots equally within sites
+stems %>%
+    group_by(model, site_id, plot_id, genus_id) %>%
+    # Count num indiv of each genus within plot
+    summarize(n=n()) %>%
+    ungroup() %>%
+    group_by(model, site_id, plot_id) %>%
+    # Convert counts to plot-level weights
+    mutate(weight=n/sum(n)) %>%
+    collect() %>%
+    group_by(model, site_id, genus_id) %>%
+    # Convert plot-level weights to site-level weights for each genus
+    summarise(weight=sum(weight)) %>%
+    group_by(model, site_id) %>%
+    # Normalize weights by site
+    mutate(weight=weight/sum(weight)) -> genus_weights_bysite
+
+# Calculate overall genus weights, weighting all sites equally
+genus_weights <- group_by(genus_weights_bysite, genus_id) %>%
+    # Sum genus weights across sites
+    summarise(weight=sum(weight)) %>%
+    # Normalize weights overall
+    mutate(weight=weight/sum(weight))
+
+# Check that weighting worked correctly (totals should be three as there are 
+# three models:
+#group_by(genus_weights, site_id) %>% summarise(sum(weight))
+
+B_g_betas <- foreach(this_model=c('tmn', 'tmp', 'tmx'), .combine=rbind,
+                     .packages=c('dplyr', 'RPostgreSQL', 'reshape2', 
+                                 'matrixStats'), .inorder=FALSE) %dopar% {
+    params <- tbl(src_postgres('tree_growth', user=pgsqluser, 
+                               password=pgsqlpwd), 'interact')
+    B_g_params <- filter(params, parameter_base == 'B_g',
+                         model == this_model) %>% 
+        rename(genus_id=row_id, param_id=col_id) %>%
+        collapse() %>%
+        mutate(param=sql("parameter_base || '_' || param_id"),
+               estimate_id=sql("chain || '_' || iteration")) %>%
+        collapse() %>%
+        select(model, param, estimate_id, genus_id, value) %>%
+        arrange(model, param, estimate_id, genus_id) %>%
         collect() %>%
         dcast(model + param + estimate_id ~ genus_id, value.var="value") %>%
         select(-estimate_id)
-    data.frame(model=these_B_g_betas$model,
-               param=paste0(these_B_g_betas$param, '_median'),
-               value=rowWeightedMedians(as.matrix(these_B_g_betas[c(-1, -2)]), w=genus_weights$weight))
+    # Remember columns 1 and 2 are the model and parameter IDs
+    stopifnot(names(these_B_g_betas)[c(-1, -2)] == genus_weights$genus_id)
+    d <- data.frame(model=these_B_g_betas$model,
+        param=paste0(these_B_g_betas$param, '_median'),
+        value=rowWeightedMedians(as.matrix(these_B_g_betas[c(-1, -2)]), 
+                                 w=genus_weights$weight))
+    return(d)
 }
+
+save(B_g_betas, 'B_g_betas_weighted.RData')
 
 B_g_labels <- c('mu_B_g[1]'='int.',
                 'mu_B_g[2]'='P',
