d6bb7262dc6014067d87c6c3c5f36155ec107cef
Alex Zvoleff <azvoleff@gmail.com>
2015-05-26 10:48:22 -0400

    Add initial temp, dbh, and precip to stems database.

--- 4_3_copy_to_db.R
+++ 4_3_copy_to_db.R
@@ -26,7 +26,12 @@ base_folder <- file.path(prefix, "TEAM", "Tree_Growth")
 # load(file.path(base_folder, "Extracted_Parameters", "parameter_estimates_correlated.RData"))
 # write.csv(params, file.path(base_folder, "Extracted_Parameters", "parameter_estimates_correlated.csv"), row.names=FALSE)
 
-make_table <- function(con, model_type) {
+foreach (model_type=c('simple', 'interact', 'correlated'),
+         .packages=c('RPostgreSQL')) %dopar% {
+    con <- dbConnect(PostgreSQL(), dbname='tree_growth', user=pgsqluser, 
+                     password=pgsqlpwd)
+
+    message(paste("Copying", model_type))
     dbSendQuery(con, paste0("DROP TABLE IF EXISTS ", model_type))
     create_qry <- paste0("CREATE TABLE ", model_type, "(
         iteration integer,
@@ -39,34 +44,20 @@ make_table <- function(con, model_type) {
         model text
     )")
     dbSendQuery(con, create_qry)
-}
 
-create_indices <- function(con, model_type) {
+    csv_file <- file.path(base_folder, "Extracted_Parameters", 
+                          paste0("parameter_estimates_", model_type, ".csv"))
+
+    dbSendQuery(con, paste0("COPY ", model_type, " FROM '", csv_file,
+                            "' WITH (FORMAT CSV, HEADER);"))
+    dbSendQuery(con, paste0("VACUUM ANALYZE ", model_type, ";"))
+
     idx_qry <- paste0("CREATE INDEX ON ", model_type, " (parameter);",
         "CREATE INDEX ON ", model_type, " (parameter_base);",
         "CREATE INDEX ON ", model_type, " (row_id);",
         "CREATE INDEX ON ", model_type, " (col_id);",
         "CREATE INDEX ON ", model_type, " (model);")
     dbSendQuery(con, idx_qry)
-}
-
-copy_csv <- function(con, csv_file, model_type) {
-    dbSendQuery(con, paste0("COPY ", model_type, " FROM '", csv_file,
-                            "' WITH (FORMAT CSV, HEADER);"))
-    dbSendQuery(con, paste0("VACUUM ANALYZE ", model_type, ";"))
-}
-
-foreach (model_type=c('simple', 'interact', 'correlated'),
-         .packages=c('RPostgreSQL')) %dopar% {
-    con <- dbConnect(PostgreSQL(), dbname='tree_growth', user=pgsqluser, 
-                     password=pgsqlpwd)
-
-    message(paste("Copying", model_type))
-    make_table(con, model_type)
-    csv_file <- file.path(base_folder, "Extracted_Parameters", 
-                          paste0("parameter_estimates_", model_type, ".csv"))
-    copy_csv(con, csv_file, model_type)
-    create_indices(con, model_type)
 }
 
 prefixes <- c('D:/azvoleff/Data', # CI-TEAM
@@ -79,10 +70,23 @@ stems <- foreach (temp_var=c('tmn', 'tmp', 'tmx'), .combine=rbind) %dopar% {
     load(file.path(base_folder, 'Data', paste0("model_data_wide_full-", 
                                                temp_var, 
                                                "_meanannual-mcwd_run12.RData")))
+
+    # Use linear indexing to calculate index of first observation for each stem
+    first_obs_row <- match(c(1:nrow(model_data$dbh)), model_data$obs_indices[, 1])
+    # First climate obs is associated with second dbh observation
+    second_obs_row <- match(c(1:nrow(model_data$dbh)), model_data$obs_indices[, 1]) + 1
+    obs_indices_linear <- (model_data$obs_indices[, 2] - 1) * nrow(model_data$dbh) + model_data$obs_indices[, 1]
+    initial_dbh <- model_data$dbh[obs_indices_linear[first_obs_row]]
+    initial_precip <- model_data$precip[obs_indices_linear[second_obs_row]]
+    initial_temp <- model_data$temp[obs_indices_linear[second_obs_row]]
+
     data.frame(model=temp_var,
                site_id=model_data$site_ID, 
                plot_id=model_data$plot_ID, 
-               genus_id=model_data$genus_ID)
+               genus_id=model_data$genus_ID,
+               initial_dbh=initial_dbh,
+               initial_precip=initial_precip,
+               initial_temp=initial_temp)
 }
 
 con <- dbConnect(PostgreSQL(), dbname='tree_growth', user=pgsqluser, password=pgsqlpwd)
--- 4_5_2_plot_weighted_coefs.R
+++ 4_5_2_plot_weighted_coefs.R
@@ -94,43 +94,6 @@ p <- caterpillar(filter(B_g_betas, param %in% paste0('B_g_', c(2:5, 8:9), '_medi
 ggsave('caterpillar_climate_interact_weighted_noD.png', p, width=plot_width*1.5, 
        height=plot_height, dpi=plot_dpi)
 
-# Temp plots
-mins <- c(11:24)
-
-# Plot Rhats for each model
-foreach(this_model=unique(params$model)) %do% {
-    pars <- filter(params, model == this_model)
-    attributes(pars)$nChains <- max(params$chain)
-    attributes(pars)$nIterations <- max(params$iteration)
-    attributes(pars)$nThin <- nThin
-    attributes(pars)$nBurnin <- nBurnin
-
-    ggs_Rhat(pars, 'B_k')
-    ggsave(paste0('Rhat_interact_B_k_', this_model, '.png'), width=plot_width*2, 
-           height=plot_height*2, dpi=plot_dpi)
-
-    ggs_traceplot(pars, 'mu_B_g')
-    ggsave(paste0('traceplot_interact_mu_B_g_', this_model, '.png'), width=plot_width*2, 
-           height=plot_height*4, dpi=plot_dpi)
-    ggs_Rhat(pars, 'mu_B_g')
-    ggsave(paste0('Rhat_interact_mu_B_g_', this_model, '.png'), width=plot_width*2, 
-           height=plot_height*2, dpi=plot_dpi)
-
-    ggs_traceplot(pars, 'sigma_')
-    ggsave(paste0('traceplot_interact_sigma_', this_model, '.png'), width=plot_width*2, 
-           height=plot_height*6, dpi=plot_dpi)
-    ggs_Rhat(pars, 'sigma_')
-    ggsave(paste0('Rhat_interact_sigma_', this_model, '.png'), width=plot_width*2, 
-           height=plot_height*2, dpi=plot_dpi)
-
-    # ggs_traceplot(pars, 'int_')
-    # ggsave(paste0('traceplot_interact_int_', this_model, '.png'), width=plot_width*2, 
-    #        height=plot_height*3, dpi=plot_dpi)
-    ggs_Rhat(pars, 'int_')
-    ggsave(paste0('Rhat_interact_int_', this_model, '.png'), width=plot_width*2, 
-           height=plot_height*3, dpi=plot_dpi)
-}
-
 # Function to make a growth prediction as a function of a particular climate 
 # variable
 make_preds <- function(B, temp, precip, dbhs) {
@@ -171,7 +134,7 @@ preds <- foreach(this_model=unique(B_g_betas$model), .combine=rbind) %do% {
     # Remember temp is standardized, so round it then center it just so the 
     # units look pretty
     temps <- round(temp_mean) - temp_mean
-    temps <- c(temps - 1, temps, temps + 1)
+    temps <- c(temps - 2, temps, temps + 2)
     temp_preds <- foreach(temp=temps, .combine=rbind) %do% {
         temp_preds <- make_preds(B, temp, (150 - precip_mean)/mm_per_unit, dbhs_centered)
         temp_preds <- cbind(Panel="Temperature", clim=temp + temp_mean, 
@@ -196,6 +159,179 @@ preds <- foreach(this_model=unique(B_g_betas$model), .combine=rbind) %do% {
 
     return(preds)
 }
+
+
+
+
+
+
+
+dbhs <- seq(10, 120, 1)
+precips <- c(7.5, 15, 22.5)
+temps <- c(10:32)
+
+# this_model <- 'tmn'
+# this_plot <- 1
+preds <- foreach(this_model=c('tmn', 'tmp', 'tmx'), .combine=rbind,
+                 .packages=c('foreach', 'dplyr', 'RPostgreSQL', 'reshape2', 
+                             'matrixStats'), .inorder=FALSE) %dopar% {
+        pg_src <- src_postgres('tree_growth', user=pgsqluser, 
+                               password=pgsqlpwd)
+        params <- tbl(pg_src, 'interact')
+        params <- filter(params, model == this_model)
+        stems <- tbl(pg_src, paste0('stems_', this_model))
+
+        # Genus-level random effects
+        B_g_params <- filter(params, parameter_base == 'B_g') %>% 
+            rename(genus_id=row_id, param_id=col_id)
+
+        # Site-level intercepts
+        int_k_params <- filter(params, parameter_base == 'int_k') %>% 
+            rename(site_id=row_id) %>% select(-col_id) %>% collect()
+
+        # Plot-level intercepts
+        int_jk_params <- filter(params, parameter_base == 'int_jk') %>% 
+            rename(plot_id=row_id) %>% select(-col_id) %>% collect()
+
+        # Elevation difference between plot and CRU cell center
+        B_k_params <- filter(params, parameter_base == 'B_k') %>% 
+            rename(site_id=row_id) %>% select(-col_id) %>% collect()
+
+
+        suffix <- paste0("_full-", this_model, "_meanannual-mcwd_run12")
+        # Need to key plot_id to site_id
+        plot_id_factor_key <- read.csv(file=file.path(data_folder,
+                                                      paste0("plot_ID_factor_key", 
+                                                             suffix, ".csv")))
+        # Need to key plot_id to site_id
+        site_id_factor_key <- read.csv(file=file.path(data_folder, 
+                                                      paste0("site_ID_factor_key", 
+                                                             suffix, ".csv")))
+        # Need elevation data
+        load(file.path(data_folder, paste0("model_data_elev_key", suffix, ".RData")))
+
+        # Need to center the predictor variables
+        load(file.path(data_folder, paste0("model_data_standardizing", suffix, 
+                                           ".RData")))
+
+        plot_id_factor_key$site_ID_char <- gsub('(VG)|([0-9]*)', '', plot_id_factor_key$plot_ID_char)
+
+        site_key <- left_join(plot_id_factor_key, rename(elev_key, plot_ID_char=plot_ID)) %>%
+            left_join(site_id_factor_key, by="site_ID_char") %>%
+            rename(plot_id=plot_ID_numeric, site_id=site_ID_numeric)
+
+        this_site <- site_key$site_id[match(this_plot, site_key$plot_id)]
+        this_site_char <- site_key$site_ID_char[match(this_plot, site_key$plot_id)]
+        this_plot_char <- site_key$plot_ID_char[match(this_plot, site_key$plot_id)]
+
+        this_elev_diff <- site_key$elev_diff[match(this_plot, site_key$plot_id)]
+
+        genus_weights <- filter(stems, plot_id == this_plot) %>%
+            group_by(genus_id) %>%
+            summarize(n=n()) %>%
+            ungroup() %>%
+            mutate(weight=n/sum(n)) %>%
+            select(-n)
+
+        # Calculate weighted median values of each parameter for each iteration 
+        # (combining the separate chains together). Each iteration has multiple 
+        # estimates of each parameter as there is one estimated parameter value 
+        # for each genus.
+        B_g_betas <- filter(left_join(genus_weights, B_g_params)) %>%
+            mutate(param=sql("parameter_base || '_' || param_id"),
+                   estimate_id=sql("chain || '_' || iteration")) %>%
+            arrange(param, estimate_id, genus_id) %>%
+            collect() %>%
+            dcast(param + estimate_id ~ genus_id, value.var="value") %>%
+            select(-estimate_id)
+        B_g_betas <- data.frame(param=paste0(B_g_betas$param, '_median'),
+                                value=rowWeightedMedians(as.matrix(B_g_betas[-1]), w=genus_weights$weight))
+
+        # Convert to matrix for linear algebra
+        B <- matrix(B_g_betas$value, nrow=length(unique(B_g_betas$param)), byrow=TRUE)
+
+        # Calculate intercept
+        intercept <- filter(int_k_params, site_id == this_site, model == this_model)$value +
+            filter(int_jk_params, plot_id == this_plot, model == this_model)$value
+
+        # Calculate elevation adjustment for this plot
+        elev_adjust <- filter(B_k_params, site_id == this_site)$value * (this_elev_diff - elev_diff_mean)
+
+        intercept <- intercept + elev_adjust
+
+        dbhs_centered <- dbhs - dbh_mean
+        temps_centered <- temps - temp_mean
+        # Change MCWD units from cm to mm
+        precips_centered <- (precips * mm_per_unit) - precip_mean
+
+        X <- foreach(dbh=dbhs_centered, .combine=rbind) %:%
+            foreach(temp=temps_centered, .combine=rbind) %:%
+            foreach(precip=precips_centered, .combine=rbind) %do% {
+                X <- matrix(1, ncol=1) # genus-level intercept
+                X <- cbind(X, precip)
+                X <- cbind(X, precip^2)
+                X <- cbind(X, temp)
+                X <- cbind(X, temp^2)
+                X <- cbind(X, dbh)
+                X <- cbind(X, dbh^2)
+                X <- cbind(X, precip*dbh)
+                X <- cbind(X, temp*dbh)
+        }
+        these_preds <- X %*% B 
+        these_preds <- these_preds + matrix(rep(intercept, nrow(X)), 
+                                            nrow=nrow(X), byrow=TRUE)
+        # X[, 6] (dbh) is subtracted so that output is growth increment
+        these_preds <- these_preds - matrix(rep(X[, 6], each=ncol(B)), 
+                                            ncol=ncol(B), byrow=TRUE)
+        medians <- apply(these_preds, 1, median)
+        q2pt5 <- apply(these_preds, 1, quantile, .025)
+        q97pt5 <- apply(these_preds, 1, quantile, .975)
+        these_preds <- data.frame(model=this_model,
+                                  plot_id=this_plot_char, 
+                                  site_id=this_site_char,
+                                  precip=(X[, 2] + precip_mean)/mm_per_unit,
+                                  temp=X[, 4] + temp_mean,
+                                  dbh=X[, 6] + dbh_mean,
+                                  median=medians,
+                                  q2pt5,
+                                  q97pt5)
+
+        head(filter(these_preds, precip == 15, temp == 20))
+
+        return(these_preds)
+}
+
+save(preds, file='growth_predictions_byplot.RData')
+
+con <- dbConnect(PostgreSQL(), dbname='tree_growth', user=pgsqluser, 
+                 password=pgsqlpwd)
+if (dbExistsTable(con, "preds_interact")) dbRemoveTable(con, "preds_interact")
+dbWriteTable(con, "preds_interact", preds)
+dbSendQuery(con, paste0("VACUUM ANALYZE preds_interact;"))
+idx_qry <- paste0("CREATE INDEX ON preds_interact (model);",
+    "CREATE INDEX ON preds_interact (plot_id);",
+    "CREATE INDEX ON preds_interact (site_id);",
+    "CREATE INDEX ON preds_interact (precip);",
+    "CREATE INDEX ON preds_interact (temp);",
+    "CREATE INDEX ON preds_interact (dbh);")
+dbSendQuery(con, idx_qry)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 preds$clim <- factor(sprintf('%.02f', preds$clim))
 preds$model <- factor(preds$model, levels=c('tmn', 'tmp', 'tmx'), 
