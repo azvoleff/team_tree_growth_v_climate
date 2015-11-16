787eb8e4771a81cd325bccc6299ec634280ac8ef
Alex Zvoleff <azvoleff@gmail.com>
2015-11-16 16:37:41 -0500

    Add growth rate monitor.

--- 3_fit_model_jags_extend.R
+++ 3_fit_model_jags_extend.R
@@ -3,3 +3,6 @@ jags_fit <- extend.jags(jags_fit, sample=1000, add.monitor=c("int_jk", "int_k", 
 jags_fit <- extend.jags(jags_fit, sample=1000, add.monitor=c("B_g"))
 
 jags_fit <- extend.jags(jags_fit, sample=400, summarise=FALSE)
+
+# Sample including latent diameters
+jags_fit <- extend.jags(jags_fit, sample=200, summarise=FALSE, add.monitor=c("dbh_latent"))
